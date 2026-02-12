2608853a2b1be45ca15d9798b14d50870a7d93db
VeronicaJimenez639 <va.jimenezm@uea.edu.ec>
2026-02-11 20:01:02 -0500

    Se agrega comentarios en Getters de Producto

--- Inventario/modelos/producto.py
+++ Inventario/modelos/producto.py
@@ -12,16 +12,19 @@ class Producto:
         self.set_precio(precio)    
 
     # Getters
-    def get_id(self) -> int:
-        return self.__id
+    # Estos métodos permiten leer los atributos privados del producto.
+    # Se usan para mantener encapsulación (no acceder directamente a __id, __nombre, etc.)
+
+    def get_id(self) -> int:    
+        return self.__id     # Devuelve el ID del producto (entero)
 
     def get_nombre(self) -> str:
-        return self.__nombre
+        return self.__nombre  # Devuelve el nombre del producto (cadena)
 
     def get_cantidad(self) -> int:
-        return self.__cantidad
+        return self.__cantidad  # Devuelve la cantidad disponible del producto (entero)
 
     def get_precio(self) -> float:
-        return self.__precio
+        return self.__precio # Devuelve el precio del producto (flotante)
 
 
